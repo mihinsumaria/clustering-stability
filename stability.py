a559b1140abaa532153746c3e3182439a5750b3d
Mihin Sumaria <mihin@ma-mihin.ma.runwaynine.com>
2019-06-11 12:07:52 -0400

    params passed to class for object init, sample_size added

--- stability.py
+++ stability.py
@@ -13,17 +13,20 @@ class Stability:
 
         Arguments:
             X {numpy.ndarray} -- original data that will be used for clustering
-             clusterer {object} -- Object similar to a sklearn.cluster class
+             clusterer {object} -- Object similar to a sklearn.cluster CLASS
              object, should have the function fit_predict, and that function
-             should return cluster labels
+             should return cluster labels, Eg. sklearn.cluster.MiniBatchKMeans,
+             not an object of MiniBatchKMeans
         """
         self.data = data
         self.clusterer = clusterer
 
-    def resample(self, number_of_resamples, dilution_factor, random_state):
+    @staticmethod
+    def resample(data, number_of_resamples, dilution_factor, random_state):
         """create resample indices of the data
 
         Arguments:
+            data {numpy.ndarray} -- data that needs to be resampled
             number_of_resamples {int} -- number of resamples to be created
             dilution_factor {float} -- a number between 0 and 1, which decides
              the number of samples in each resample
@@ -34,12 +37,12 @@ class Stability:
             resamples {list} -- list of resamples
         """
         np.random.seed(random_state)
-        sample_size = int(dilution_factor * self.data.shape[0])
-        resample_indices = [np.random.choice(np.arange(self.data.shape[0]),
+        sample_size = int(dilution_factor * data.shape[0])
+        resample_indices = [np.random.choice(np.arange(data.shape[0]),
                                              size=sample_size,
                                              replace=False) for i in
                             range(number_of_resamples)]
-        resamples = [self.data[indices] for indices in resample_indices]
+        resamples = [data[indices] for indices in resample_indices]
 
         return resample_indices, resamples
 
@@ -89,36 +92,47 @@ class Stability:
         return matrix
 
     def compute_stability_score(self, number_of_resamples, dilution_factor,
-                                random_state=None, params=None,
-                                original_labels=None, n_jobs=None):
+                                params, random_state=None, original_labels=None, 
+                                n_jobs=None, sample_size=None):
         """Computes stability score for a given set of params
 
         Arguments:
             number_of_resamples {int} -- number of resamples to be created
             dilution_factor {float} -- a number between 0 and 1, which decides
              the number of samples in each resample
+            params {dict} -- dictionary containing parameters required for 
+             self.clusterer object
 
         Keyword Arguments:
             random_state {int} -- if int, random_state is the seed used by the
              random number generator (default: {None})
-            params {dict} -- dictionary containing function parameters
-             corresponding to self.clusterer.fit_predict (default: {None})
             original_labels {numpy.ndarray} -- original labels for the data 
              given at the time of initialization for the given clusterer. If 
              not passed, then its recomputed (default: {None})
             n_jobs {int} -- number of jobs to run in parallel for matrix 
              creation, refer create_connectivity_matrix docstring 
              (default: {None})
+            sample_size {int} -- number of samples to consider when computing 
+             score, not to be confused with the number of resamples. The
+             resamples will be drawn from this sample. (default: {None})
 
         Returns:
             {float} -- a stability score for the clustering algorithm with the
              given set of parameters for self.data
         """
         if original_labels is None:
-            original_labels = self.clusterer.fit_predict(self.data, **params)
-        original_mat = Stability.create_connectivity_matrix(original_labels,
-                                                            n_jobs)
-        sample_indices, samples = self.resample(number_of_resamples,
+            original_labels = self.clusterer(**params).fit_predict(self.data)
+        if sample_size:
+            label_indices = np.random.choice(np.arange(self.data.shape[0]), 
+                                             size=sample_size,
+                                             replace=False)
+            data = self.data[label_indices]
+            labels = original_labels[label_indices]
+        else:
+            data = self.data
+            labels = original_labels
+        original_mat = Stability.create_connectivity_matrix(labels, n_jobs)
+        sample_indices, samples = self.resample(data, number_of_resamples,
                                                 dilution_factor, random_state)
         scores = []
         for i, sample in enumerate(samples):
